db11cf333557df8294f7804f63fd569d0fcbf0e5
charliefj <mrcharliefj@gmail.com>
2026-07-07 16:00:37 +1000

    3: GameLoop, playerClass, some shape drawing

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
 def main():
+    #initialise pygame
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    #screen object from display class, setting size
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    #create an object to help track time, helps fps
+    clock = pygame.time.Clock()
+    dt= 0.0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    #game loop
     while True:
+        #logs data in a jsonl file for bootdev to confirm it works
         log_state()
+
+        #closes the game loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+    
+        #every frame makes screen black, prevents smearing
         screen.fill("Black")
+
+        player.draw(screen)
+
 
         pygame.display.flip()
 
-        
-    print(f"Starting Asteroids with my pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000    
 
 if __name__ == "__main__":
     main()
